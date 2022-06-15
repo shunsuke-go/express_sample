8f287ffaf369244542f01825db3e3d20d68a44de
gon tana <gontana1220@gmail.com>
2022-06-15 14:56:31 +0900

    パスワード拡張用の準備

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,8 +1,18 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient, Prisma } from '@prisma/client'
 
 const prisma = new PrismaClient()
 export class UserService {
-  static async getUser() {
+  static async all() {
     return await prisma.user.findMany()
+  }
+
+  static async find(id: number) {
+    return await prisma.user.findUnique({
+      where: { id }
+    })
+  }
+
+  static async create(name: string, email: string, hasedPassword: string) {
+    await prisma.user.create({ data: { name, email, hasedPassword } })
   }
 }
