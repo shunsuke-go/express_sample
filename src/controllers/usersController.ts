ccf8d4dcefb746b51bd1d43a8e32f273d2e426f6
gon tana <gontana1220@gmail.com>
2022-06-15 14:55:22 +0900

    routes から controllersに変更

---
+++ src/controllers/usersController.ts
@@ -0,0 +1,40 @@
+import { UserService } from '~/services/UserService'
+import bcrypt from 'bcrypt'
+
+import { Request, Response, Router } from 'express'
+
+export const userRouter = Router()
+
+userRouter.get('/', async (req: Request, res: Response) => {
+  const allUsers = await UserService.all()
+  return res.status(200).send({
+    users: allUsers.map(user => ({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+    }))
+  })
+})
+
+userRouter.get('/:id', async (req: Request, res: Response) => {
+  const user = await UserService.find(parseInt(req.params?.id))
+  return res.status(200).send({
+    user: {
+      id: user.id,
+      name: user.name,
+      email: user.email
+    }
+  })
+})
+
+userRouter.post('/', async (req: Request, res: Response) => {
+  const { name, email, password }: { name?: string, email?: string, password?: string } = req.body
+  const hasedPassword = bcrypt.hashSync(password, 10)
+
+  try {
+    await UserService.create(name, email, hasedPassword)
+    return res.status(200)
+  } catch(e) {
+    console.error(e)
+  }
+})
