f24881d5889c86cfd4ad041188c8d21d33891ff7
gon tana <gontana1220@gmail.com>
2022-06-16 10:52:19 +0900

    jwt認証用関数作成

--- src/helpers/verifyToken.ts
+++ src/helpers/verifyToken.ts
@@ -1,7 +1,25 @@
+import jwt from 'jsonwebtoken'
 import { Request, Response, NextFunction } from 'express'
+import { UserService } from '~/services/UserService';
 
 export const verifyToken = async (req: Request, res: Response, next: NextFunction) => {
+  const authHeader = req.headers["authorization"];
+  if (!authHeader) return res.json({ message: 'no header' })
+  if (authHeader.split(" ")[0] !== "Bearer") return res.json({ message: 'header format error' })
 
-
-  next()
+  try {
+    const token = jwt.verify(authHeader.split(" ")[1], process.env.JWT_SECRET)
+    if (typeof token === 'string') return
+    if (!token.id) return
+    const user = await UserService.find({ id: parseInt(token.id) })
+    if (user) {
+      res.locals.user = user
+      next()
+    } else {
+      res.json({ error: "auth error" })
+    }
+  } catch (e) {
+    console.log(e.message)
+    res.json({ error: e.message })
+  }
 }
