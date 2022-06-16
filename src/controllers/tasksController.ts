5fafbd0993c40b290004646183efaeb3c8d786ce
gon tana <gontana1220@gmail.com>
2022-06-16 10:52:31 +0900

    TaskのCRUD完成

--- src/controllers/tasksController.ts
+++ src/controllers/tasksController.ts
@@ -1,11 +1,53 @@
 import { Request, Response, Router } from 'express'
-import bcrypt from 'bcrypt'
 import { verifyToken } from '~/helpers/verifyToken'
-import jwt from 'jsonwebtoken'
-import { UserService } from '~/services/UserService'
+import { Task, User } from '@prisma/client'
+import { TaskService } from '~/services/TaskService'
 
 export const taskRouter = Router()
 
-taskRouter.get('/', verifyToken, () => {
+taskRouter.get('/', verifyToken, async (req: Request, res: Response) => {
+  const user: User = res.locals.user
+  const tasks: Task[] = await TaskService.all({ userId: user.id })
 
+  res.status(200).send({ user: user, tasks: tasks })
+})
+
+taskRouter.get('/:id', verifyToken, async (req: Request, res: Response) => {
+  try {
+    const task: Task = await TaskService.find({ id: parseInt(req.params.id) })
+    res.status(200).send({ task })
+  } catch (e) {
+    res.status(404).send({ message: 'not found'})
+  }
+})
+
+taskRouter.post('/', verifyToken, async (req: Request, res: Response) => {
+  const user: User = res.locals.user
+  console.log("aaaaaaaaaa")
+  const { title, description, isDone }: Task = req.body
+  try {
+    await TaskService.create({ title, description, isDone, userId: user.id })
+    res.status(201).send({ message: 'created'})
+  } catch (e){
+    res.status(403).send({ message: 'post error'})
+  }
+})
+
+taskRouter.delete('/:id', verifyToken, async (req: Request, res: Response) => {
+  try {
+    await TaskService.delete({ id: parseInt(req.params.id) })
+    res.status(200).send()
+  } catch (e) {
+    res.status(400).send({ message: 'delete error'})
+  }
+})
+
+taskRouter.patch('/:id', verifyToken, async (req: Request, res: Response) => {
+  const { title, description, isDone } = req.body
+  try {
+    await TaskService.update({ id: parseInt(req.params.id), data: { title, description, isDone }})
+    res.status(200).send()
+  } catch (e) {
+    res.status(400).send({ message: 'delete error'})
+  }
 })
--- src/services/TaskService.ts
+++ src/services/TaskService.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient, Task } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
@@ -13,7 +13,12 @@ export class TaskService {
     })
   }
 
-  static async create(title: string, description: string, isDone: boolean, userId: number) {
+  static async create({
+    title,
+    description,
+    isDone,
+    userId,
+  }: { title: string, description: string, isDone: boolean, userId: number }) {
     await prisma.task.create({
       data: {
         title,
@@ -22,5 +27,13 @@ export class TaskService {
         userId
       }
     })
+  }
+
+  static async delete({ id }: { id: number }) {
+    return await prisma.task.delete({ where: { id } })
+  }
+
+  static async update({ id, data }: { id: number, data: {} }) {
+    return await prisma.task.update({ where: { id }, data })
   }
 }
